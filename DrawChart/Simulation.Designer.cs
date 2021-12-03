b9c3aeb5ea212f20dee5d2a511f9e98af0ed44c5
Bassam Gamal <bassam.gamal@gmail.com>
2021-12-03 16:23:29 +0100

    Add missing files

---
+++ DrawChart/Simulation.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace DrawChart
+{
+    partial class Simulation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.start = new System.Windows.Forms.Button();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(247, 84);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(86, 45);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(3, 195);
+            this.chart.Name = "chart";
+            this.chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(599, 317);
+            this.chart.TabIndex = 1;
+            this.chart.Text = "Result";
+            // 
+            // Simulation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.start);
+            this.Name = "Simulation";
+            this.Size = new System.Drawing.Size(605, 512);
+            this.Load += new System.EventHandler(this.Simulation_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
---
+++ DrawChart/Simulation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace DrawChart
+{
+    public partial class Simulation : UserControl
+    {
+        public Simulation()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            this.chart.Series.Clear();
+            const int numberOfGames = 10000;
+            var maxBudget = Math.Pow(10, 7);
+            var gameScores = new List<double>();
+            var random = new Random();
+            for (var i = 0; i < numberOfGames; i++)
+            {
+                gameScores.Add(SimulateStPeterParadox(maxBudget, 0, random));
+            }
+
+            Draw(gameScores);
+        }
+
+        private void Draw(List<double> gameScores)
+        {
+            for (var i = 0; i < gameScores.Count; i++)
+            {
+                var series = this.chart.Series.Add($"Game {i}");
+                series.Points.Add(gameScores[i]);
+            }
+
+            chart.ChartAreas[0].RecalculateAxesScale();
+
+        }
+
+        private static double SimulateStPeterParadox(double maxBudget, int round, Random random)
+        {
+            while (true)
+            {
+                var currentPower = Math.Pow(2, round);
+                if (currentPower > maxBudget) return maxBudget;
+
+                if (random.Next(0, 2) == 0)
+                    return currentPower;
+
+                round++;
+            }
+        }
+
+        private void Simulation_Load(object sender, EventArgs e)
+        {
+            this.chart.Series.Clear();
+            chart.Titles.Add("Simulation Result");
+        }
+    }
+}
